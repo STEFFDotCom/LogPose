1175291247ac9349b7c5f383be718ed7805031cb
Steffen Andersen <steff@telenor.dk>
2026-06-11 23:08:15 +0200

    strated work on auth.py creating the login route

--- app/blueprints/auth/auth.py
+++ app/blueprints/auth/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, request, render_template
 
 auth = Blueprint("auth", __name__)
 
@@ -6,7 +6,17 @@ auth = Blueprint("auth", __name__)
 # POST the user fills out the login, hits submit, credentials gets sent to the server
 @auth.route("/login", methods = ["GET", "POST"])
 def login():
-    pass
+    # show login page
+    if request.method == "GET":
+        return render_template("login.html")
+    
+    # read submitted data and check it against DB
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        print(username, password)
+        return render_template("Logged in with success")
 
 # GET user navigates to page and gets served empty form to create user
 # POST user fills out form and we sent it to the server to create the user
