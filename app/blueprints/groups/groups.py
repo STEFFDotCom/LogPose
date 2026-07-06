506a8a97ecbc3de65f511cdfd4e8b0c11be15ea2
Steffen Andersen <steff@telenor.dk>
2026-07-06 10:31:14 +0200

    finished edit group and also added some more comments to group blueprint. briefly started on links blueprint

--- app/blueprints/groups/groups.py
+++ app/blueprints/groups/groups.py
@@ -27,6 +27,7 @@ def create_group(board_id):
             flash("You do not have permission to view this - contact admin", "danger")
             return redirect(url_for("boards.my_boards"))
     
+    # create the group
     new_group_name = request.form.get("GROUP_NAME_PLACEHOLDER")
     new_group_color = request.form.get("GROUP_COLOR_PLACEHOLDER")
     new_group_board_id = get_board_row.id
@@ -42,29 +43,35 @@ def create_group(board_id):
 @login_required
 def delete_group(group_id):
 
+    # get row of pressed group
     get_group_row = Group.query.filter_by(id = group_id).first()
 
+    # check if group exists and fetch the board id
     if get_group_row:
         board_id = get_group_row.board_id
     else:
         flash("Group does not exist", "danger")
         return redirect(url_for("boards.my_boards"))
 
+    # get the entire board row
     get_board_row = Board.query.filter_by(id = board_id).first()
     
+    # DEFENSIVE GUARD
+    # make sure board exists, if board is shared that user is have editor role and if personal board that it is the correct user - then delete board
     if not get_board_row:
         flash("Board does not exist or an error happened - contact admin", "danger")
         return redirect(url_for("boards.my_boards"))
     elif get_board_row.is_shared == True:
         get_userteam_row = UserTeam.query.filter_by(user_id = current_user.id, team_id = get_board_row.team_id).first()
         if get_userteam_row.role != "editor":
-            flash("You do not have permission to delete this board. If you want to have it deleted, contact admin.", "danger")
+            flash("You do not have permission to delete this group. If you want to have it deleted, contact admin.", "danger")
             return redirect(url_for("boards.view_board", board_id = board_id))
     elif get_board_row.is_shared == False:
         if get_board_row.user_id != current_user.id:
-            flash("You do not have permission to delete this board. If you want to have it deleted, contact admin.", "danger")
+            flash("You do not have permission to delete this group. If you want to have it deleted, contact admin.", "danger")
             return redirect(url_for("boards.view_board", board_id = board_id))
     
+    # delete the group
     db.session.delete(get_group_row)
     db.session.commit()
     flash("Group has been successfully deleted!", "success")
@@ -74,4 +81,46 @@ def delete_group(group_id):
 @groups.route("/groups/<int:group_id>/edit", methods = ["POST"])
 @login_required
 def edit_group(group_id):
-    pass
+
+    # get row of pressed group
+    get_group_row = Group.query.filter_by(id = group_id).first()
+
+    # make sure the group exists
+    if get_group_row:
+        board_id = get_group_row.board_id
+    else:
+        flash("Group does not exist", "danger")
+        return redirect(url_for("boards.my_boards"))
+    
+    get_board_row = Board.query.filter_by(id = board_id).first()
+
+    # permission check / defensive guard
+    if not get_board_row:
+        flash("Board does not exist or an error happened - contact admin", "danger")
+        return redirect(url_for("boards.my_boards"))
+    elif get_board_row.is_shared == True:
+        get_userteam_row = UserTeam.query.filter_by(user_id = current_user.id, team_id = get_board_row.team_id).first()
+        if get_userteam_row.role != "editor":
+            flash("You do not have permission to edit this board. If you believe this is a mistake contact admin.", "danger")
+            return redirect(url_for("boards.view_board", board_id = board_id))
+    elif get_board_row.is_shared == False:
+        if get_board_row.user_id != current_user.id:
+            flash("You do not have permission to delete this board. If you want to have it deleted, contact admin.", "danger")
+            return redirect(url_for("boards.view_board", board_id = board_id))
+    
+    # get new name and new color and commit it to DB
+    new_name = request.form.get("NEW_NAME_PLACEHOLDER")
+
+    if not new_name:
+        flash("Group name cannot be empty", "danger")
+        return redirect(url_for("boards.view_board", board_id = board_id))
+    
+    get_group_row.group_name = new_name
+
+    new_color = request.form.get("NEW_COLOR_PLACEHOLDER")
+    get_group_row.group_color = new_color
+
+    db.session.commit()
+
+    flash("Group has been successfully updated!", "success")
+    return redirect(url_for("boards.view_board", board_id = board_id))
--- app/blueprints/links/links.py
+++ app/blueprints/links/links.py
@@ -1,12 +1,19 @@
-from flask import Blueprint
-from flask_login import login_required
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
 from app import db
+from app.models import Board, UserTeam, Group
 
 links = Blueprint("links", __name__)
 
 @links.route("/boards/<int:board_id>/links/create", methods = ["POST"])
 @login_required
 def create_link(board_id):
+    
+    # get row of pressed board
+    get_board_row = Board.query.filter_by(id = board_id).first()
+    
+
+
     pass
 
 @links.route("/links/<int:link_id>/delete", methods = ["POST"])
